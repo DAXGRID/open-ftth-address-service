d1ccd504d0bbce329520c606bc8a43d432e2b488
Jesper Ladegaard <jesper@dax.dk>
2021-08-21 22:00:10 +0200

    fix so that providing the same address id multiple times in a search don't throw an application exception

--- OpenFTTH.Address.Business/QueryHandling/GetAddressInfoQueryHandler.cs
+++ OpenFTTH.Address.Business/QueryHandling/GetAddressInfoQueryHandler.cs
@@ -1,4 +1,5 @@
 ﻿using FluentResults;
+using Microsoft.Extensions.Logging;
 using OpenFTTH.Address.API.Model;
 using OpenFTTH.Address.API.Queries;
 using OpenFTTH.CQRS;
@@ -12,10 +13,12 @@ namespace OpenFTTH.Address.Business.QueryHandling
     public class GetAddressInfoQueryHandler
         : IQueryHandler<GetAddressInfo, Result<GetAddressInfoResult>>
     {
+        private readonly ILogger<GetAddressInfoQueryHandler> _logger;
         private readonly IAddressRepository _addressRepository;
 
-        public GetAddressInfoQueryHandler(IAddressRepository addressRepository)
+        public GetAddressInfoQueryHandler(ILogger<GetAddressInfoQueryHandler> logger, IAddressRepository addressRepository)
         {
+            _logger = logger;
             _addressRepository = addressRepository;
         }
 
@@ -44,9 +47,6 @@ namespace OpenFTTH.Address.Business.QueryHandling
                     accessAddresses.Add(addressSearchItem.Item2.Id, (AccessAddress)addressSearchItem.Item2);
                 else if (addressSearchItem.Item2 is UnitAddress && !unitAddresses.ContainsKey(addressSearchItem.Item2.Id))
                     unitAddresses.Add(addressSearchItem.Item2.Id, (UnitAddress)addressSearchItem.Item2);
-                else
-                    throw new ApplicationException($"Don't know how to handle address entity type: {addressSearchItem.Item2.GetType().FullName}");
-
             }
 
             var result = Result.Ok<GetAddressInfoResult>(new GetAddressInfoResult(hits.ToArray(), accessAddresses.Values.ToArray(), unitAddresses.Values.ToArray()));
